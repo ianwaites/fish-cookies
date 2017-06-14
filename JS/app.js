3f1adf9a618f6dbd16709f683c19a06f7e9e45cc
Ian Waites <ian.waites@gmail.com>
2017-06-14 10:15:48 -0700

    added a couple of headers to the table

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var allLocations = [];
 var theTable = document.getElementById('pike');
 var hourlyTotals = [];
@@ -46,19 +46,24 @@ function calcHourlyTotals() {
   }
 };
 
+// Push hours to table header
 var renderHeader = function() {
   var trEL = document.createElement('tr');
   var thEL = document.createElement('th');
-  thEL.textContent = '';
+  thEL.textContent = 'Locations';
   trEL.appendChild(thEL);
   for (var i = 0; i < hours.length; i++) {
     var thEL = document.createElement('th');
     thEL.textContent = hours[i];
     trEL.appendChild(thEL);
   }
+  thEL = document.createElement('th');
+  thEL.textContent = 'Daily';
+  trEL.appendChild(thEL);
   theTable.appendChild(trEL);
 };
 
+// Push totals to TD's in DOM
 CookieStore.prototype.render = function() {
   var trEL = document.createElement('tr');
   var tdEL = document.createElement('td');
@@ -70,8 +75,12 @@ CookieStore.prototype.render = function() {
     tdEL.textContent = this.cookiesEachHour[i];
     trEL.appendChild(tdEL);
   }
+  // tdEl = document.createElement('td');
+  // tdEL.textContent = this.totalDaily;
+  // theTable.appendChild(tdEl);
   theTable.appendChild(trEL);
 };
+
 // Footer TOTALLLLL
 function renderFooter() {
   var tfootEL = document.createElement('tfoot');
